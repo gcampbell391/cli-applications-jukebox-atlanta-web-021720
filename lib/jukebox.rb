fdc70dec9a2eed9b7b0311552fae8844c791f33e
Gene Campbell III <gcampbell391@gmail.com>
2020-02-14 02:55:24 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -50,7 +50,7 @@ def run(songs)
 	help
 	input = nil
 	while input != "exit"
-		puts "Please enter a command:"
+		puts "I accept the following commands:"
 		input = gets.chomp
 		case
 		when input == "help"
